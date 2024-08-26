9ca48f8b81360bb401e734ce234f5d452a52b55e
Daniel <daniel01mcs@gmail.com>
2024-08-26 15:01:56 +0200

    Add files via upload

---
+++ Project/WindowsFormsApp1/Presentation/Login.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lkCreate = new System.Windows.Forms.LinkLabel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lkCreate
+            // 
+            this.lkCreate.AutoSize = true;
+            this.lkCreate.Location = new System.Drawing.Point(216, 299);
+            this.lkCreate.Name = "lkCreate";
+            this.lkCreate.Size = new System.Drawing.Size(104, 17);
+            this.lkCreate.TabIndex = 11;
+            this.lkCreate.TabStop = true;
+            this.lkCreate.Text = "Create account";
+            this.lkCreate.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lkCreate_LinkClicked);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(59, 285);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 31);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(125, 224);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(130, 22);
+            this.txtPass.TabIndex = 9;
+            // 
+            // txtUName
+            // 
+            this.txtUName.Location = new System.Drawing.Point(125, 109);
+            this.txtUName.Name = "txtUName";
+            this.txtUName.Size = new System.Drawing.Size(130, 22);
+            this.txtUName.TabIndex = 8;
+            this.txtUName.TextChanged += new System.EventHandler(this.txtUName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter User Name";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 375);
+            this.Controls.Add(this.lkCreate);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel lkCreate;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Project/WindowsFormsApp1/Presentation/Login.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Login : Form
+    {
+        string path = "login.txt";
+        string UserName;
+        string Password;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            UserName = txtUName.Text;
+            Password = txtPass.Text;
+            bool found = false;
+
+            foreach (string line in File.ReadLines(path))
+            {
+                string[] sections = line.Split(' ');
+                if (sections.Length == 2)
+                {
+                    string storedName = sections[0];
+                    string storedPassword = sections[1];
+
+                    if (UserName == storedName && Password == storedPassword)
+                    {
+                        MessageBox.Show("Succesfully logged in");
+                        Windows_GUI main = new Windows_GUI();
+                        main.Show();
+                        this.Hide();                       
+                        found = true;
+                        return;
+                    }
+                }
+            }
+
+            if (found == false)
+            {
+                MessageBox.Show("Incorrect Username or Password");
+            }
+        }
+
+        private void lkCreate_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            UserName = txtUName.Text;
+            Password = txtPass.Text;
+
+            if (Exists(UserName))
+            {
+                MessageBox.Show("User already exists");
+                return;
+            }
+
+            NewUser(UserName, Password);
+            MessageBox.Show("User succesfully added");
+
+            Windows_GUI main = new Windows_GUI();
+            main.Show();
+            this.Hide();
+        }
+
+        private bool Exists(string uname)
+        {
+            return File.ReadLines(path).Any(line => line.StartsWith(uname + " "));
+        }
+
+        private void NewUser(string uname, string password)
+        {
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                writer.WriteLine($"{uname} {password}");
+            }
+        }
+
+        private void txtUName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
